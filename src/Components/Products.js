7c117d05ed8aad2bf81477d5768c61cb34b7200b
karthi <karthi@byzerotechnologies.com>
2022-05-09 19:07:47 +0530

    main

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -3,6 +3,7 @@ import Footer from './Footer'
 import medium1 from '../img/ss-angle.png'
 import medium2 from '../img/ss-bolts.png'
 import medium3 from '../img/ss-tube.png'
+import ImageZoom from "react-medium-image-zoom";
 
 export default function Products() {
   return (
@@ -10,34 +11,36 @@ export default function Products() {
     <div className='container'>
       <div className='row'>
         <div className='col-md-4 col-sm-4 col-xs-12'>
-        <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src={medium1} alt="Card image cap" />
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div className="card" style="width: 18rem;">
+  <img className="card-img-top" src={medium1} alt="Card image cap" />
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div>
         </div>
         <div className='col-md-4 col-sm-4 col-xs-12'>
-        <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src={medium2} alt="Card image cap" />
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <ImageZoom>
+        <div className="card" style="width: 18rem;">
+  <img className="card-img-top" src={medium2} alt="Card image cap" />
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div>
+</ImageZoom>
         </div>
         <div className='col-md-4 col-sm-4 col-xs-12'>
-        <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src={medium3} alt="Card image cap" />
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div className="card" style="width: 18rem;">
+  <img className="card-img-top" src={medium3} alt="Card image cap" />
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div>
         </div>
       </div>
     </div>
     <div><Footer /></div>
-    
     </>
+    
   )
 }
 
