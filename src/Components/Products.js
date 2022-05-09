4532172efd80b0c25c211b9dc53feb3e45306c16
karthi <karthi@byzerotechnologies.com>
2022-05-09 19:02:02 +0530

    main

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,33 +1,43 @@
 import React from 'react'
-import Footer from "./Footer";
-import zomimg from "../img/image-1.jpg"
+import Footer from './Footer'
+import medium1 from '../img/ss-angle.png'
+import medium2 from '../img/ss-bolts.png'
+import medium3 from '../img/ss-tube.png'
 
 export default function Products() {
   return (
     <>
-<div className='container'>
-  <div className='row'>
-  <div className='col-md-4'>
-<figure>
-          <img
-            id="zoom-background"
-            src={zomimg}
-            alt="Zoom with background color"
-          />
-          <figcaption>Zoom with background color</figcaption>
-        </figure>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-4 col-sm-4 col-xs-12'>
+        <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src={medium1} alt="Card image cap" />
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
 </div>
-<div className='col-md-4'>
-
+        </div>
+        <div className='col-md-4 col-sm-4 col-xs-12'>
+        <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src={medium2} alt="Card image cap" />
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
 </div>
-<div className='col-md-4'>
-  
+        </div>
+        <div className='col-md-4 col-sm-4 col-xs-12'>
+        <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src={medium3} alt="Card image cap" />
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
 </div>
-</div>
-</div>
-    <div><Footer />
+        </div>
+      </div>
     </div>
-  
+    <div><Footer /></div>
+    
     </>
   )
 }
+
