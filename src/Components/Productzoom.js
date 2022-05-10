fc9d3e1a48c635c8a4b3123cf8255aba68e05403
karthi <karthi@byzerotechnologies.com>
2022-05-10 11:15:30 +0530

    main

--- src/Components/Productzoom.js
+++ src/Components/Productzoom.js
@@ -9,20 +9,23 @@ const Productzoom = () => (
     <>
     <div className='container'>
         <div className='row'>
-            <div className='col-md-4 col-sm-6 col-xs-12'>
-            <Zoom>
-                  <img alt="img1" src={medium1} width="200" />
-            </Zoom>
+           <div className='col-md-4 col-sm-6 col-xs-12'>
+                  <Zoom>
+                      <img alt="img1" src={medium1} width="200" />
+                  </Zoom>
+                  <h5>SS - Angles</h5>
             </div>
             <div className='col-md-4 col-sm-6 col-xs-12'>
                <Zoom>
                    <img alt="img1" src={medium2} width="200" />
                 </Zoom>
+                 <h5>SS - Angles</h5>
             </div>
             <div className='col-md-4 col-sm-6 col-xs-12'>
             <Zoom>
                   <img alt="img1" src={medium3} width="200" />
             </Zoom>
+            <h5>SS - Angles</h5>
             </div>
         </div>
     </div>
