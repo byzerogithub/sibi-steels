9e5707fd89b36cdc43aa5e0fde900b5e8911b88d
karthi <karthi@byzerotechnologies.com>
2022-05-10 11:39:18 +0530

    main

--- src/Components/Productzoom.js
+++ src/Components/Productzoom.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Zoom from 'react-medium-image-zoom'
+import "../Components/Product.css"
 import 'react-medium-image-zoom/dist/styles.css'
 import medium1 from '../img/ss-angle.png'
 import medium2 from '../img/ss-bolts.png'
@@ -7,28 +8,30 @@ import medium3 from '../img/ss-tube.png'
 
 const Productzoom = () => (
     <>
-    <div className='container'>
-        <div className='row'>
+    <div className='prod-zoom'>
+      <div className='container'>
+        <div className='row product-zoom'>
            <div className='col-md-4 col-sm-6 col-xs-12'>
                   <Zoom>
-                      <img alt="img1" src={medium1} width="200" />
+                      <img alt="img1" src={medium1} width="100%" />
                   </Zoom>
                   <h5>SS - Angles</h5>
             </div>
             <div className='col-md-4 col-sm-6 col-xs-12'>
                <Zoom>
-                   <img alt="img1" src={medium2} width="200" />
+                   <img alt="img1" src={medium2} width="100%" />
                 </Zoom>
                  <h5>SS - Angles</h5>
             </div>
             <div className='col-md-4 col-sm-6 col-xs-12'>
             <Zoom>
-                  <img alt="img1" src={medium3} width="200" />
+                  <img alt="img1" src={medium3} width="100%" />
             </Zoom>
             <h5>SS - Angles</h5>
             </div>
         </div>
     </div>
+</div>
     </>
   
 )
