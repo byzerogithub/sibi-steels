dffba84d10d81a3efd7e906bcc2a3bac5409eee6
Gowtham <gowtham@byzerotechnologies.com>
2022-12-07 11:57:51 +0530

    main

---
+++ src/Components/Pages/Contact_page/nodemail.jsx
@@ -0,0 +1,124 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./Contact.css";
+import Footer from "../Footer_page/Footer";
+export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_ocl5u4s",
+        "template_xjdinc6",
+        form.current,
+        "xahhGGlABhjCzqyZz"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <>
+      <div class="banner">
+        <div className="banner-inn">
+          <h2>Contact us</h2>
+        </div>
+      </div>
+
+      <div className="cont-sec">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-sm-12 ol-xs-12 cont-seca">
+              <div class="col align-self-center">
+                <h1 class="text-center">Get In Touch</h1>
+                {/* <!-- contact form --> */}
+                <form>
+                  {/* <!-- name --> */}
+                  <div class="form-group">
+                    <label for="name">Name</label>
+                    <input
+                      type="name"
+                      name="name"
+                      class="form-control"
+                      id="name"
+                    />
+                  </div>
+
+                  {/* <!-- email --> */}
+                  <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input
+                      type="email"
+                      name="email"
+                      class="form-control"
+                      id="email"
+                    />
+                  </div>
+
+                  {/* <!-- subject --> */}
+                  <div class="form-group">
+                    <label for="subject">Mobile No</label>
+                    <input
+                      type="tel"
+                      name="subject"
+                      class="form-control"
+                      id="subject"
+                    />
+                  </div>
+
+                  <div class="form-group">
+                    <label for="email_body">Message</label>
+                    <textarea
+                      class="form-control"
+                      id="email_body"
+                      rows="5"
+                    ></textarea>
+                  </div>
+
+                  <button type="submit" class="btn btn-primary">
+                    Submit
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className="col-md-6 col-sm-12 ol-xs-12 cont-secb">
+              <div className="add-1">
+                <h5>Salem</h5>
+                <address>
+                  <p>
+                    {" "}
+                    No 28/5, Seetharaman Road, Near Chatram,
+                    <br /> Salem -636009, Tamilnadu, India.
+                  </p>
+                </address>
+                <a href="tel:+914272353822">+91 - 427 - 2353822</a> <br />
+                <a href="mailto:sibisteels@gmail.com">sibisteels@gmail.com</a>
+              </div>
+              <div className="add-1">
+                <h5>Erode</h5>
+                <address>
+                  <p>
+                    39/74 KNK Road, Karungalpalayam,
+                    <br /> Erode -638 003, Tamilnadu, India.
+                  </p>
+                </address>
+
+                <a href="tel:+914242222822">+91 - 424 - 2222822</a>
+                <br />
+                <a href="mailto:sibisteels@gmail.com">sibisteels@gmail.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+    </>
+  );
+}
